1e0e0b783e1603ee4f37c9efc6f5459609340536
adritac-tech <adrita.chakraborty@outlook.com>
2022-03-08 10:10:01 -0800

    fixing method

--- IndividualChallenges/Matrix.java
+++ IndividualChallenges/Matrix.java
@@ -1,31 +1,28 @@
-public class Matrix {
-    private final int[][] matrix;
-
+public class Matrix(int[][] matrix) {
     // store matrix
-    public Matrix(int[][] matrix) {
-        this.matrix = matrix;
-    }
 
     // Hack: create toString method using nested for loops to format output of a matrix
-    public toString(int[][] matrix){
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[].length; j++){
+    public String[][] toString(int[][] matrix) {
+       // String[][] matrixupdated =
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
                 matrix[i][j] = Integer.toString(matrix[i][j]);
             }
         }
+        return matrix;
     }
 
 
     // declare and initialize a matrix for a keypad
     static int[][] keypad() {
-        return new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, {-1, 0, -1} };
+        return new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {-1, 0, -1}};
     }
 
     // declare and initialize a random length arrays
     static int[][] numbers() {
-        return new int[][]{ { 0, 1 },
-                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
+        return new int[][]{{0, 1},
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}};
     }
 
     // tester method for matrix formatting
